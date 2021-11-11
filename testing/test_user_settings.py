b80deab53eb81635e6bdec50f6308dc2e0970ae7
Mitchell Tyson <mitchell.tyson@intersystems.com>
2021-11-10 19:42:09 -0500

    user screening unit tests done

--- testing/test_user_settings.py
+++ testing/test_user_settings.py
@@ -2,9 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from .config import URL
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
 
 def test_webpage_up():
-    print(f'test webpage is up')
     service=Service('chromedriver.exe')
     driver = webdriver.Chrome(service=service)
     
@@ -15,7 +16,6 @@ def test_webpage_up():
     assert title == driver.title
  
 def test_nav_to_user_settings():
-    print(f'test nav to user settings')
     service=Service('chromedriver.exe')
     driver = webdriver.Chrome(service=service)
     
@@ -27,3 +27,57 @@ def test_nav_to_user_settings():
 
     state_dropdown = driver.find_element(By.NAME, "select-state-dropdown")
     assert state_dropdown.is_displayed()  
+
+def test_valid_state_found():
+    service=Service('chromedriver.exe')
+    driver = webdriver.Chrome(service=service)
+    
+    driver.get(URL)
+    driver.maximize_window()
+    
+    go_to_quote_button = driver.find_element(By.NAME, "btn_get_a_quote")
+    go_to_quote_button.click()
+
+    state_dropdown = driver.find_element(By.NAME, "select-state-dropdown")
+    state_dropdown.click()
+
+    texas_option = driver.find_element(By.NAME, "state-option-TX")
+    assert texas_option.is_displayed()  
+
+def test_invalid_state_not_found():
+    service=Service('chromedriver.exe')
+    driver = webdriver.Chrome(service=service)
+    
+    driver.get(URL)
+    driver.maximize_window()
+    
+    go_to_quote_button = driver.find_element(By.NAME, "btn_get_a_quote")
+    go_to_quote_button.click()
+
+    state_dropdown = Select(driver.find_element(By.NAME, "select-state-dropdown"))
+    state_options = state_dropdown.options
+    state_options_states = [state_option.text for state_option in state_options]
+
+    assert "Michigan" not in state_options_states
+
+
+def test_nav_to_quote_after_continue():
+    service=Service('chromedriver.exe')
+    driver = webdriver.Chrome(service=service)
+    
+    driver.get(URL)
+    driver.maximize_window()
+    
+    go_to_quote_button = driver.find_element(By.NAME, "btn_get_a_quote")
+    go_to_quote_button.click()
+
+    state_dropdown = Select(driver.find_element(By.NAME, "select-state-dropdown"))
+    state_dropdown.select_by_value('CA')
+
+    continue_button = driver.find_element(By.NAME, "btn-continue-to-quote")
+    continue_button.click()
+
+    current_url = driver.current_url
+
+    assert current_url.endswith('quote')
+
