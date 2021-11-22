e150e8d2ed7fab2317fda7826c11e43e4c0e3a34
Mitchell Tyson <mitchell.tyson@intersystems.com>
2021-11-22 18:42:48 -0500

    shell file test

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-pwd
-ls
-echo "Hello World"
-cd functional_testing
-pytest
+string="Get a Quote, Get Covered, Today"
+if curl -s "https://insurancemaster.herokuapp.com/" | grep -q "$string"; then
+    echo "Insurance Master is up and running"
+    exit 0
+else
+    echo "Insurance Master is not up and running. Investigate Now!"
+    exit 1
+fi
