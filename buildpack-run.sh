4e6afda5678a7af50f464e23f29c3162b8144b85
Mitchell Tyson <mitchell.tyson@intersystems.com>
2021-11-23 08:53:51 -0500

    testing fail post test

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Testing if website is up and running"
 string_to_find="Get a Quote, Get Covered, Today"
-if curl -s "https://insurancemaster.herokuapp.com/" | grep -q "$string_to_find"; then
+if curl -s "https://insurancemaster.herokuap.com/" | grep -q "$string_to_find"; then
     echo "Insurance Master is up and running"
     exit 0
 else
