6ad3e64a13dc759eeb5b86946d41859a85b1b202
Mitchell Tyson <mitchell.tyson@intersystems.com>
2021-11-23 14:36:18 -0500

    validation on date 30 days into future

--- static/js/quotebusiness.js
+++ static/js/quotebusiness.js
@@ -9,3 +9,22 @@ function displayOptionTextField() {
   }
 }
 
+function checkValidDate() {
+  var selectedDate = document.getElementById("startpicker").value;
+
+  if (isNot30DaysAhead(selectedDate)) {
+    alert("Date is not 30 days or more into the future");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function isNot30DaysAhead(selectedDate) {
+  var today = new Date();
+  var newSelectedDate = new Date(selectedDate);
+
+  daysDifference =
+    (newSelectedDate.getTime() - today.getTime()) / (1000 * 3600 * 24);
+  return daysDifference < 30;
+}
