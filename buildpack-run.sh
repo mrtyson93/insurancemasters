45d6bcefec34ab7d16fd4c5da1363c4e81cf6b49
Mitchell Tyson <mitchell.tyson@intersystems.com>
2021-11-28 15:20:41 -0500

    I hope this definitely fails a pipeline

--- buildpack-run.sh
+++ buildpack-run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "Testing if website is up and running"
+pytest
 string_to_find="Get a Quote, Get Covered, Today"
 if curl -s "https://insurancemaster.herokuapp.com/" | grep -q "$string_to_find"; then
     echo "Insurance Master is up and running"
